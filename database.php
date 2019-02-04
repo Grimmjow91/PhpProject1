658225544c5818c6317f535fb147bc96fcd4b05d
Nathan Premo <Nathan Premo@DESKTOP-UDMG239>
2019-02-04 12:16:10 -0500

    changed database connection information

--- database.php
+++ database.php
@@ -3,16 +3,25 @@ class Database
 {
 	// these are not the real passwords
 	// for real passwords, see file in ../database subdirectory
-	/*private static $dbName = 'crud_tutorial' ; 
+	
+        //000webhost.com
+        private static $dbName = 'id8586639_crud_tutorial' ; 
+	private static $dbHost = 'localhost' ;
+	private static $dbUsername = 'id8586639_grimm';
+	private static $dbUserPassword = 'DankMemesTaco';
+    
+        
+        //local
+        /*private static $dbName = 'crud_tutorial' ; 
 	private static $dbHost = 'localhost' ;
 	private static $dbUsername = 'root';
 	private static $dbUserPassword = '';*/
 	
-        
-        private static $dbName = 'gpcorser355wi19' ; 
-	private static $dbHost = '10.8.30.49' ;
-	private static $dbUsername = 'gpcorser355wi19';
-	private static $dbUserPassword = '';
+        //csis server
+        /*private static $dbName = 'nmpremo1355wi19' ;
+    private static $dbHost = '10.8.30.49' ;
+    private static $dbUsername = 'nmpremo1355wi19';
+    private static $dbUserPassword = 'grimmjow91';*/
     
 	private static $cont  = null;
 	
